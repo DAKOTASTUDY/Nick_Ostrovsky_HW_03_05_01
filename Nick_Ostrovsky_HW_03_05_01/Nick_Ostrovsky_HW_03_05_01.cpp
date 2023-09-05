a7e45112fb76b26dd4d5aad5e9168823c053998c
Nick Ostrovsky <legatus357@gmail.com>
2023-09-05 13:17:32 -0700

    name/get_name added.

--- Nick_Ostrovsky_HW_03_05_01/Nick_Ostrovsky_HW_03_05_01.cpp
+++ Nick_Ostrovsky_HW_03_05_01/Nick_Ostrovsky_HW_03_05_01.cpp
@@ -5,9 +5,10 @@ class Figure
 {
 protected:
     int side_count;
+    std::string name;
 public:
-    Figure() : side_count(0) {}
-    Figure(int s_c) : side_count(s_c) {}
+    Figure() : side_count(0), name("Figure") {}
+    Figure(int s_c, std::string n) : side_count(s_c), name(n) {}
     int get_side()
     {
         return side_count;
@@ -17,13 +18,17 @@ public:
        
         return get_side();
     }
+    std::string get_name()
+    {
+        return name;
 
+    }
 };
 
 class Triangle : public Figure
 {
 public:
-    Triangle() : Figure(3) {};
+    Triangle() : Figure(3, "Triangle") {};
 
 
 
@@ -31,7 +36,7 @@ public:
 class Quad : public Figure
 {
 public:
-    Quad() : Figure(4) {};
+    Quad() : Figure(4, "Quad") {};
 
 
 
@@ -41,9 +46,9 @@ int main()
     Figure a;
     Triangle b;
     Quad c;
-    std::cout << "\n" << "Figure: \t" << a.side_out() << std::endl;
-    std::cout << "\n" << "Triangle: \t" << b.side_out() << std::endl;
-    std::cout << "\n" << "Quad: \t\t" << c.side_out() << std::endl;
+    std::cout << "\n" << a.get_name() << "\t" << a.side_out() << std::endl;
+    std::cout << "\n" << b.get_name() << "\t" << b.side_out() << std::endl;
+    std::cout << "\n" <<c.get_name() << "\t\t" << c.side_out() << std::endl;
     return 0;
 }
 
